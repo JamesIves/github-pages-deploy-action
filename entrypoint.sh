81a7bf3c85b53fa23812f67a20ea510e483ae140
James Ives <iam@jamesiv.es>
2019-03-03 12:10:31 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,7 +20,7 @@ cd $GITHUB_WORKSPACE && \
 
 # Configures Git and checks out the base branch.
 git config --global user.email "${COMMIT_EMAIL:-gh-pages-deploy@jives.dev}" && \
-git config --global user.name "${COMMIT_NAME:-Github Pages Deploy}" && \
+git config --global user.name "${COMMIT_NAME:-Github Pages Deployer}" && \
 git checkout "${BASE_BRANCH:-master}" && \
 
 # Builds the project if applicable.
