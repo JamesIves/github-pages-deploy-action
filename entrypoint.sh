752f33ab7d9ec22bcd6ae76cafc6f3466bf22130
James Ives <iam@jamesiv.es>
2019-03-04 10:28:36 -0500

    Changing value

--- entrypoint.sh
+++ entrypoint.sh
@@ -45,7 +45,7 @@ REPOSITORY_PATH="https://${ACCESS_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" &&
 
 # Checks to see if the remote exists prior to deploying.
 # If the branch doesn't exist it gets created here as an orphan.
-if [ `git ls-remote --heads "https://${ACCESS_TOKEN}@github.com:JamesIves/reddit-viewer.git" $BRANCH | wc -l` ]
+if [ `git ls-remote --heads $REPOSITORY_PATH $BRANCH | wc -l` ]
 then
   echo "Creating remote branch ${BRANCH} as it doesn't exist..."
   git checkout --orphan $BRANCH && \
