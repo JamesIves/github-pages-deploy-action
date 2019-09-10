63a55de371940ce1400cdcdb575adf9187d66f14
James Ives <iam@jamesiv.es>
2019-09-10 14:35:42 -0400

    q

--- entrypoint.sh
+++ entrypoint.sh
@@ -78,8 +78,8 @@ fi
 
 # Commits the data to Github.
 echo "Deploying to GitHub..." && \
-git add -f --quiet $FOLDER && \
+git add -f $FOLDER && \
 
-git commit -m "Deploying to ${BRANCH} - $(date +"%T")" && \
+git commit -m -q "Deploying to ${BRANCH} - $(date +"%T")" && \
 git push $REPOSITORY_PATH `git subtree split --prefix $FOLDER ${BASE_BRANCH:-master}`:$BRANCH --force && \
 echo "Deployment succesful!"
