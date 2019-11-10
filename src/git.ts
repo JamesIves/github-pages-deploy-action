3843226fed0da2c1886121c304c8fe8165c267a7
James Ives <iam@jamesiv.es>
2019-11-10 15:08:04 -0500

    Debugging

--- src/git.ts
+++ src/git.ts
@@ -87,7 +87,7 @@ export async function deploy(): Promise<any> {
   );
 
   await execute(
-    `git push origin ${action.branch}`,
+    `git push --force origin ${action.branch}`,
     action.build
   );
 
