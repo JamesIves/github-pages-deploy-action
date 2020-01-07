00c27584153f0775b85a9975b749f62bfa4ea8d4
James Ives <iam@jamesiv.es>
2020-01-07 17:33:55 -0500

    hotfix-1

--- src/git.ts
+++ src/git.ts
@@ -22,7 +22,6 @@ export async function init(): Promise<any> {
     await execute(`git init`, workspace);
     await execute(`git config user.name ${action.name}`, workspace);
     await execute(`git config user.email ${action.email}`, workspace);
-    await execute(`git fetch origin`, workspace);
   } catch (error) {
     core.setFailed(`There was an error initializing the repository: ${error}`);
   } finally {
@@ -89,7 +88,8 @@ export async function deploy(): Promise<any> {
 
   // Checks out the base branch to begin the deployment process.
   await switchToBaseBranch();
-  await execute(`git fetch origin`, workspace);
+  await execute(`git fetch ${repositoryPath}`, workspace);
+  await execute(`git pull`, workspace)
   await execute(
     `git worktree add --checkout ${temporaryDeploymentDirectory} origin/${action.branch}`,
     workspace
