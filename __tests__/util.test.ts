50cde99bc8e4ad2f0ec7aa1902a090fe1b154220
JamesIves <iam@jamesiv.es>
2020-01-18 14:04:28 -0500

    Removes lodash

--- __tests__/util.test.ts
+++ __tests__/util.test.ts
@@ -1,21 +1,20 @@
-import {isNullOrUndefined} from '../src/util';
+import { isNullOrUndefined } from "../src/util";
 
-
-describe('util', () => {
-  describe('isNullOrUndefined', () => {
-    it('should return true if the value is null', async() => {
+describe("util", () => {
+  describe("isNullOrUndefined", () => {
+    it("should return true if the value is null", async () => {
       const value = null;
-      expect(isNullOrUndefined(value)).toBeTruthy()
+      expect(isNullOrUndefined(value)).toBeTruthy();
     });
 
-    it('should return true if the value is undefined', async() => {
+    it("should return true if the value is undefined", async () => {
       const value = undefined;
-      expect(isNullOrUndefined(value)).toBeTruthy()
+      expect(isNullOrUndefined(value)).toBeTruthy();
     });
 
-    it('should return false if the value is defined', async() => {
-      const value = 'montezuma';
-      expect(isNullOrUndefined(value)).toBeFalsy()
+    it("should return false if the value is defined", async () => {
+      const value = "montezuma";
+      expect(isNullOrUndefined(value)).toBeFalsy();
     });
-  })
-})
+  });
+});
