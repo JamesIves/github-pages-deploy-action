3527f6281d040df9276f836d224fd660636ab537
JamesIves <iam@jamesiv.es>
2020-02-27 07:20:27 -0500

    Compiling

--- lib/util.d.ts
+++ lib/util.d.ts
@@ -1,5 +1,8 @@
+import { actionInterface } from "./constants";
 /** Utility function that checks to see if a value is undefined or not.
  * @param {*} value = The value to check.
  * @returns {boolean}
  */
 export declare function isNullOrUndefined(value: any): boolean;
+export declare const generateTokenType: (action: actionInterface) => string;
+export declare const generateRepositoryPath: (action: actionInterface) => string;
