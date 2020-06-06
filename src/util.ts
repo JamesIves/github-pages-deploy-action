c6c36e7358170ea857346292160d1625b9779603
James Ives <iam@jamesiv.es>
2020-06-06 13:18:49 -0400

    Test2

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,5 @@
 import {isDebug} from '@actions/core'
-import {existsSync} from 'fs'
+// import {existsSync} from 'fs'
 import {ActionInterface} from './constants'
 
 /* Utility function that checks to see if a value is undefined or not. */
@@ -52,11 +52,11 @@ export const hasRequiredParameters = (action: ActionInterface): void => {
     )
   }
 
-  if (!existsSync(action.folder) && action.folder !== action.root) {
-    throw new Error(
-      `The ${action.folder} directory you're trying to deploy doesn't exist. ❗`
-    )
-  }
+  // if (!existsSync(action.folder) && action.folder !== action.root) {
+  //   throw new Error(
+  //     `The ${action.folder} directory you're trying to deploy doesn't exist. ❗`
+  //   )
+  // }
 }
 
 /* Suppresses sensitive information from being exposed in error messages. */
