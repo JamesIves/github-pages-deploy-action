a5a215d3182f0f4b45dff94a185f366f7baffc0e
James Ives <iam@jamesiv.es>
2020-06-06 12:27:02 -0400

    Revert

---
+++ node_modules/@octokit/plugin-paginate-rest/node_modules/@octokit/types/dist-node/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+const VERSION = "2.16.2";
+
+exports.VERSION = VERSION;
+//# sourceMappingURL=index.js.map
---
+++ node_modules/@octokit/plugin-rest-endpoint-methods/node_modules/@octokit/types/dist-web/index.js
@@ -0,0 +1,4 @@
+const VERSION = "2.16.2";
+
+export { VERSION };
+//# sourceMappingURL=index.js.map
