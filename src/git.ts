9f1f563e2b1495c31d1d0f66a567712218e11623
James Ives <iam@jamesiv.es>
2020-09-27 13:46:45 -0400

    Preservation Changes

--- src/git.ts
+++ src/git.ts
@@ -29,12 +29,18 @@ export async function init(action: ActionInterface): Promise<void | Error> {
       action.silent
     )
 
-    await execute(`git remote rm origin`, action.workspace, action.silent)
+    await execute(`git remote rm origin`, action.workspace, action.silent) || true;
     await execute(
       `git remote add origin ${action.repositoryPath}`,
       action.workspace,
       action.silent
     )
+
+    if (action.preserve) {
+      info(`Stashing workspace changes… ⬆️`)
+      await execute(`git stash`, action.workspace, action.silent);
+    }
+
     await execute(
       `git fetch --no-recurse-submodules`,
       action.workspace,
@@ -163,6 +169,11 @@ export async function deploy(action: ActionInterface): Promise<Status> {
         action.workspace,
         action.silent
       )
+    }
+
+    if (action.preserve) {
+      info(`Applying stashed workspace changes… ⬆️`)
+      await execute(`git stash apply`, action.workspace, action.silent) || true;
     }
 
     await execute(
