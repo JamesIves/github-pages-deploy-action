0b8cf6deeacc838b157bc5bc60a3b52199a4ea25
James Ives <iam@jamesiv.es>
2021-11-18 09:31:10 -0500

    Resolves a merge issue

--- lib/git.d.ts
+++
@@ -1,3 +0,0 @@
-import { ActionInterface, Status } from './constants';
-export declare function init(action: ActionInterface): Promise<void | Error>;
-export declare function deploy(action: ActionInterface): Promise<Status>;
--- lib/worktree.d.ts
+++
@@ -1,9 +0,0 @@
-import { ActionInterface } from './constants';
-export declare class GitCheckout {
-    orphan: boolean;
-    commitish?: string | null;
-    branch: string;
-    constructor(branch: string);
-    toString(): string;
-}
-export declare function generateWorktree(action: ActionInterface, worktreedir: string, branchExists: unknown): Promise<void>;
