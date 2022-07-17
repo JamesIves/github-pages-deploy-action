6a0cc4d0b55664f70525a6ef90009e54ff7af2f8
James Ives <iam@jamesiv.es>
2022-07-17 10:14:25 -0400

    chore: Format

--- src/git.ts
+++ src/git.ts
@@ -317,7 +317,6 @@ export async function deploy(action: ActionInterface): Promise<Status> {
     info(`Changes committed to the ${action.branch} branch… 📦`)
 
     if (action.tag) {
-      console.log('ADDING TAG!!!')
       info(`Adding '${action.tag}' tag to the commit…`)
       await execute(
         `git tag ${action.tag}`,
