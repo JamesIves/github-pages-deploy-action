4bcb0dacb6b13c279e55b2a8cd3ddd6271611a09
James Ives <iam@jamesiv.es>
2024-09-27 15:13:16 +0000

    Deploy Production Code for Commit c30eaca6255fe9a52a0769888b2d63c3e79a25bb 🚀

--- lib/execute.d.ts
+++ lib/execute.d.ts
@@ -1,5 +1,14 @@
+/**
+ * The output of a command.
+ */
 type ExecuteOutput = {
+    /**
+     * The standard output of the command.
+     */
     stdout: string;
+    /**
+     * The standard error of the command.
+     */
     stderr: string;
 };
 /** Wrapper around the GitHub toolkit exec command which returns the output.
@@ -12,6 +21,12 @@ type ExecuteOutput = {
  * on a non-zero exit status or to leave implementation up to the caller.
  */
 export declare function execute(cmd: string, cwd: string, silent: boolean, ignoreReturnCode?: boolean): Promise<ExecuteOutput>;
+/**
+ * Writes the output of a command to the stdout buffer.
+ */
 export declare function stdout(data: Buffer | string): void;
+/**
+ * Writes the output of a command to the stderr buffer.
+ */
 export declare function stderr(data: Buffer | string): void;
 export {};
