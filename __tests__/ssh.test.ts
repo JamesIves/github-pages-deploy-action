f74c3c9acf412d0ab5fcf0600e748102d5aa3f93
safeengineauto <safeengineauto@gmail.com>
2026-07-05 06:19:56 +0400

    Update GitHub SSH known host keys (#1982)
    * Update SSH known hosts configuration
    
    * Add tests for SSH known hosts handling

--- __tests__/ssh.test.ts
+++ __tests__/ssh.test.ts
@@ -7,6 +7,9 @@ import {execute} from '../src/execute.js'
 import {configureSSH} from '../src/ssh.js'
 
 const originalAction = JSON.stringify(action)
+const expectedGitHubKnownHostRsa = `\ngithub.com ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCj7ndNxQowgcQnjshcLrqPEiiphnt+VTTvDP6mHBL9j1aNUkY4Ue1gvwnGLVlOhGeYrnZaMgRK6+PKCUXaDbC7qtbW8gIkhL7aGCsOr/C56SJMy/BCZfxd1nWzAOxSDPgVsmerOBYfNqltV9/hWCqBywINIR+5dIg6JTJ72pcEpEjcYgXkE2YEFXV1JHnsKgbLWNlhScqb2UmyRkQyytRLtL+38TGxkxCflmO+5Z8CSSNY7GidjMIZ7Q4zMjA2n1nGrlTDkzwDCsw+wqFPGQA179cnfGWOWRVruj16z6XyvxvjJwbz0wQZ75XK5tKSb7FNyeIEs4TT4jk+S4dhPeAUC5y+bDYirYgM4GC7uEnztnZyaVWQ7B381AK4Qdrwt51ZqExKbQpTUNn+EjqoTwvqNj4kqx5QUCI0ThS/YkOxJCXmPUWZbhjpCg56i+2aB6CmK2JGhn57K5mj0MNdBXA4/WnwH6XoPWJzK5Nyu2zB3nAZp+S5hpQs+p1vN1/wsjk=\n`
+const expectedGitHubKnownHostEcdsa = `\ngithub.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=\n`
+const expectedGitHubKnownHostEd25519 = `\ngithub.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl\n`
 
 jest.mock('fs', () => ({
   appendFileSync: jest.fn(),
@@ -75,6 +78,23 @@ describe('configureSSH', () => {
     expect(execFileSync).toHaveBeenCalledTimes(1)
     expect(exportVariable).toHaveBeenCalledTimes(2)
     expect(execSync).toHaveBeenCalledTimes(3)
+    expect(appendFileSync).toHaveBeenCalledTimes(3)
+expect(appendFileSync).toHaveBeenCalledWith(
+  `${process.env['HOME']}/.ssh/known_hosts`,
+  expectedGitHubKnownHostRsa
+)
+expect(appendFileSync).toHaveBeenCalledWith(
+  `${process.env['HOME']}/.ssh/known_hosts`,
+  expectedGitHubKnownHostEcdsa
+)
+expect(appendFileSync).toHaveBeenCalledWith(
+  `${process.env['HOME']}/.ssh/known_hosts`,
+  expectedGitHubKnownHostEd25519
+)
+expect(appendFileSync).not.toHaveBeenCalledWith(
+  expect.any(String),
+  expect.stringContaining('ssh-dss')
+)
   })
 
   it('should not export variables if the return from ssh-agent is skewed', async () => {
